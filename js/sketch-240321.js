705c6067a1b8fac5e49e487a4439a42be9a506df
snow8485 <sc8485@nyu.edu>
2024-03-27 16:42:31 +0800

    11

--- js/sketch-240321.js
+++ js/sketch-240321.js
@@ -3,28 +3,46 @@ let seq = 0;
 let cam;
 //let button;
 
-let snap;
 
-let wordsss;
-let words;
+var snap;
+
+
+var wordsss
+var words
+
+
+
+
+
 
 function preload() {
   img = loadImage('assets/start.png');
+
+
 }
+
 
 function setup() {
   let canvas = createCanvas(1063, 1890);
   canvas.parent("p5-canvas-container");
 
+
   cam = createCapture(VIDEO);
+
+
+
   cam.hide();
 
+
   snap = createImage(640 * 3.9375, 480 * 3.9375);
-  createTextInput();
+
+
 }
+
 
 function draw() {
   background(0);
+
 
   if (seq == 0) {
     background(255, 0, 0);
@@ -35,17 +53,13 @@ function draw() {
   else if (seq == 2) {
 
     push();
-
-    // to flip
     translate(width, 0);
     scale(-1, 1);
 
-    // to place the camera image to the center
-    translate(-640 * 3.9375 / 2 + 1063 / 2, 0); // - camWidth/2 + canvasWidth/2
-
-    // display the cam image and snapshot!
     image(cam, 0, 0, 640 * 3.9375, 480 * 3.9375);
-    image(snap, 0, 0, 640 * 3.9375, 480 * 3.9375);
+
+
+    image(snap, 0, 0, 640 * 3.9375, 480 * 3.9375)
 
     pop();
   }
@@ -54,19 +68,25 @@ function draw() {
   }
 }
 
+
 function updateText() {
   wordsss.html(words.value());
 }
 
+
 function keyPressed() {
 
+
   //function mousePressed() {
+
 
   if (key == " ") {
     seq = seq + 1;
   }
 
+
   if (key == "a") {
+
 
     // save('myCanvas.png');
     snap = cam.get(0, 0);
@@ -74,33 +94,56 @@ function keyPressed() {
     return false;
   }
 
+
   if (key == "p") {
 
-    //
+
+    words = createInput('say something');
+    words.position(2000, 2000);
+    createP()
+    wordsss = createP('?')
+
+
+    //wordsss.position(1000, 2200);
+
+
+    wordsss.style('font-size', 1000)
+    wordsss.style('color', 'deeppink');
+    wordsss.position(2000, 2000);
+
+
+    words.input(updateText);
   }
 
+
   if (key == "s") {
+
 
     saveCanvas('myCanvas.png');
     //snap = cam.get(0, 0);
 
     return false;
   }
+
+
+
+
+
+
 }
 
 
-function createTextInput() {
-  words = createInput('say something');
-  words.input(updateText);
-  words.parent("text-input-container")
-}
 
-function createParagraph() {
-  createP();
-  wordsss = createP('?')
-  wordsss.style('font-size', 16)
-  wordsss.style('color', 'deeppink');
-}
+
+
+
+
+
+
+
+
+
+
 
 
 
--- js/sketch.js
+++ js/sketch.js
@@ -4,11 +4,17 @@ let SCREEN_RATIO = 1;
 let img;
 let seq = 0;
 let cam;
-//let button;
+
+let button1, button2, button3, button4;
+let button5;
+
+let emoji1;
 
 let snap;
 
+
 let userTextInput = "";
+
 
 
 // TEXT OBJ
@@ -19,9 +25,13 @@ let words;
 
 function preload() {
   img = loadImage('assets/start.png');
+
+  //emoji1 = loadImage();
+
 }
 
 function setup() {
+
   let canvas = createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
   canvas.parent("p5-canvas-container");
 
@@ -31,10 +41,28 @@ function setup() {
   cam.hide();
 
   snap = createImage(640 * SCREEN_RATIO, 480 * SCREEN_RATIO);
-  createTextInput();
 
+  createTextInput();
   textX = width / 2;
   textY = height / 2;
+
+
+  button2 = createButton('next');
+
+  button2.mousePressed(NEXT);
+
+
+  button3 = createButton('Snap');
+
+  button3.mousePressed(SNAP);
+  text(userTextObj, textX, textY);
+  button3.hide();
+
+  button4 = createButton('Save');
+  button4.mousePressed(SAVE);
+  button4.hide();
+
+
 }
 
 function draw() {
@@ -62,9 +90,18 @@ function draw() {
     image(snap, 0, 0, 640 * SCREEN_RATIO, 480 * SCREEN_RATIO);
 
     pop();
+
+    button3.show();
+
+    button4.show();
+
   }
+
   else if (seq == 3) {
-    background(255, 0, 255);
+    //background(255, 0, 255);
+    image(snap, 0, 0);
+
+
   }
 
   // input
@@ -83,25 +120,27 @@ function draw() {
 
 }
 
-function keyPressed() {
-  if (key == " ") {
-    //seq = seq + 1;
-  }
-  else if (key == "a") {
-    // save('myCanvas.png');
-    //snap = cam.get(0, 0);
-  }
-  else if (key == "p") {
-    //
-  }
-  else if (key == "s") {
-    //saveCanvas('myCanvas.png');
-    //snap = cam.get(0, 0);
-  }
+function NEXT() {
 
+  seq = seq + 1;
 
 }
 
+
+function SNAP() {
+
+  snap = cam.get(0, 0);
+  //text(userTextObj, textX, textY);
+  return false;
+
+}
+
+function SAVE() {
+
+  saveCanvas('myCanvas.png');
+  return false;
+
+}
 
 
 function createTextInput() {
